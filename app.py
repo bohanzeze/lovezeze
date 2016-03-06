fcf95f23d211e59633f63a0cc5376e8850ebc131
Bohan <bohanzhang@foxmail.com>
2016-03-06 12:38:49 +0800

    完成逻辑

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import functools
+import json
 
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, flash
 from redis import StrictRedis
 import hashlib
+
+from qiniu import Auth, put_data
+import qiniu.config
+import uuid
 
 app = Flask(__name__)
 app.config.update({
     'SECRET_KEY': 'lkasjflasjdfzxcnvmn'
 })
+
+#需要填写你的 Access Key 和 Secret Key
+access_key = '1H3USr1wF7hQ80AeRlq_BF0KoEnoJq2atE4UULwp'
+secret_key = 'awFedibl6FB3L-4FSXG1NY4Qq3MFwiDoZcNFDKTF'
+
+#构建鉴权对象
+q = Auth(access_key, secret_key)
+bucket_name = 'bohan'
 
 redis_client = StrictRedis(decode_responses=True)
 
@@ -26,7 +39,31 @@ def login_required(f):
 @app.route('/')
 @login_required
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    username = request.form['username']
+    password = request.form['password']
+    md5 = hashlib.md5()
+    md5.update(password.encode())
+    encoded_password = str(md5.hexdigest())
+
+    user = redis_client.hgetall('users:' + username)
+    if not user:
+        redis_client.hmset('users:' + username, {
+            'password': encoded_password,
+            'username': username
+        })
+        flash('注册成功!', category='success')
+        return redirect('/login')
+    else:
+        flash('用户名已存在!', category='error')
+        return render_template('register.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -43,22 +80,54 @@ def login():
     user = redis_client.hgetall('users:' + username)
     if user and encoded_password == user.get('password'):
         session['user'] = user
+        flash('登录成功!', category='success')
         return redirect('/')
     else:
+        flash('用户名或密码错误!', category='error')
         return render_template('login.html')
 
 
-@app.route('/api/story', methods=['POST'])
+@app.route('/story', methods=['GET', 'POST'])
 @login_required
 def create_story():
-    pass
+    user = session.get('user')
+
+    if request.method == 'POST':
+        description = request.form.get('description')
+        f = request.files['file']
+        # upload file to qiniu
+        key = str(uuid.uuid4()) + '.jpg'
+        token = q.upload_token(bucket_name, key, 3600)
+        ret, info = put_data(token, key, f)
+        print(ret, info)
+        url = 'http://7d9pyw.com1.z0.glb.clouddn.com/' + key
+
+        story_id = redis_client.incr('users:%s:stories:next-id' % user.get('username'))
+        redis_client.hmset('users:%s:stories:%s' % (user.get('username'), story_id), {
+            'url': url,
+            'description': description
+        })
+        redis_client.rpush('users:%s:stories' % user.get('username'), story_id)
+        flash('上传成功!', category='success')
+        return redirect('/')
+    else:
+        return render_template('create_story.html')
 
 
-@app.route('/api/stories', methods=['POST'])
+@app.route('/api/stories', methods=['GET'])
 @login_required
 def get_stories():
+    page = int(request.args.get('page', 1))
+    size = 10
+
     user = session.get('user')
-    return redis_client.lrange('users:%s:stories' % user.get('name'), 0, -1)
+    story_ids = redis_client.lrange('users:%s:stories' % user.get('username'), (page - 1) * size, page * size)
+    stories = []
+    for story_id in story_ids:
+        story = redis_client.hgetall('users:%s:stories:%s' % (user.get('username'), story_id))
+        stories.append(story)
+
+    return json.dumps(stories)
 
 
 if __name__ == '__main__':
